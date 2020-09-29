116d344fd6b0569efbdc9bf87e9e974107b0a2e4
floriangrv <florian.gravepro@gmail.com>
2020-09-29 16:03:30 +0200

    add exo19

---
+++ exo_19.rb
@@ -0,0 +1,19 @@
+
+
+liste_email = []
+liste_email_even = []
+i = 00
+
+
+while i < 50
+  i = i + 1
+  if i % 2 == 0
+    liste_email_even << "jean.dupont.0#{i}@email.fr"
+  
+  end
+end
+
+
+
+
+puts liste_email_even
